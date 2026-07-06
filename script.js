49d1426fb8e45a1f97e4697f90e569804b17362f
Atlanq093 <laura.lewtak@gmail.com>
2026-07-06 16:44:53 +0200

    js part 2

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 const choices = ["rock", "paper", "scissors"];
 const playerDisplay =document.getElementById ("playerDisplay")
 const computerDisplay = document.getElementById ("computerDisplay")
-const resultDisplay =document.getElementById("reseultDisplay")
-
+const resultDisplay =document.getElementById("resultDisplay")
+let result = "";
 function playGame(playerChoice){
     const computerChoice = choices [Math.floor(Math.random()*3)];
-    let result = ""
      if (playerChoice ===computerChoice){
-        result = "It's a tie"
+       result = "It's a tie"
      }else{ 
         switch(playerChoice){
             case "rock ":
@@ -26,4 +25,17 @@ function playGame(playerChoice){
 playerDisplay.textContent = `Player: ${playerChoice}`;
 computerDisplay.textContent = `Computer: ${computerChoice}`;
 resultDisplay.textContent = result ;
+
+
+
+resultDisplay.classList.remove("greenText","redText");
+
+switch (result){
+    case "You WIN":
+        resultDisplay.classList.add("greenText")
+        break;
+        case "YOU LOOSE":
+            resultDisplay.classList.add("redText");
+            break;
+}
 }
